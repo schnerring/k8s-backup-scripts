20b122e4fba38dc2adb69887fb4c4a6c587f561f
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-01-22 04:10:32 +0100

    Add backup.sh

---
+++ backup.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+BACKUP_FILE_MAX_AGE_DAYS=30
+
+##################################################
+# Clean files older than BACKUP_FILE_MAX_AGE_DAYS days.
+# from BACKUP_DIR
+# Globals:
+#   BACKUP_FILE_MAX_AGE_DAYS
+# Arguments:
+#   Directory to clean
+##################################################
+cleaup() {
+  find "$1" -mtime "+${BACKUP_FILE_MAX_AGE_DAYS}" -type f -delete
+}
+
+REMARK_POD_LABEL="app=remark42"
+REMARK_NAMESPACE="remark42"
+REMARK_BACKUP_DIR="/mnt/backup-k8s"
+
+##################################################
+# Copy automatic Remark42 backup files from pod.
+# Globals:
+#   REMARK_POD_LABEL
+#   REMARK_NAMESPACE
+#   REMARK_BACKUP_DIR
+# Arguments:
+#   None
+##################################################
+backup_remark42() {
+  REMARK_POD_LABEL="app=remark42"
+  REMARK_NAMESPACE="remark42"
+  # -o name doesn't work because "kubectl cp" doesn't support the "pod/" prefix
+  pod=$(kubectl get pod -l "${REMARK_POD_LABEL}" -n "${REMARK_NAMESPACE}" -o jsonpath="{.items[0].metadata.name}")
+  kubectl cp "${REMARK_NAMESPACE}/${pod}:var/backup" "${REMARK_BACKUP_DIR}"
+}
+
+##################################################
+# Main function of script.
+# Globals:
+#   REMARK_BACKUP_DIR
+# Arguments:
+#   None
+##################################################
+main() {
+  backup_remark42 || exit 1
+  cleanup "${REMARK_BACKUP_DIR}" || exit 1
+}
+
+# Entrypoint
+main
