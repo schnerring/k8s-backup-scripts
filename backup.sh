171c2d8a2438b797cb2cc2b4be59656a20d4e93e
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-01-22 04:54:13 +0100

    Add Postgres backup

--- backup.sh
+++ backup.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+# Constants
+
 BACKUP_FILE_MAX_AGE_DAYS=30
+
+REMARK_POD_LABEL="app=remark42"
+REMARK_NAMESPACE="remark42"
+REMARK_BACKUP_DIR="/mnt/backup-k8s/remark42"
+
+POSTGRES_POD_LABEL="app=postgres"
+POSTGRES_NAMESPACE="postgres"
+POSTGRES_BACKUP_DIR="/mnt/backup-k8s/postgres"
 
 ##################################################
 # Clean files older than BACKUP_FILE_MAX_AGE_DAYS days.
@@ -13,10 +23,6 @@ BACKUP_FILE_MAX_AGE_DAYS=30
 cleanup() {
   find "$1" -mtime "+${BACKUP_FILE_MAX_AGE_DAYS}" -type f -delete
 }
-
-REMARK_POD_LABEL="app=remark42"
-REMARK_NAMESPACE="remark42"
-REMARK_BACKUP_DIR="/mnt/backup-k8s/remark42"
 
 ##################################################
 # Copy automatic Remark42 backup files from pod.
@@ -35,15 +41,34 @@ backup_remark42() {
 }
 
 ##################################################
+# Backup all Postgres databases.
+# Globals:
+#   POSTGRES_POD_LABEL
+#   POSTGRES_NAMESPACE
+#   POSTGRES_BACKUP_DIR
+# Arguments:
+#   None
+##################################################
+backup_postgres() {
+  mkdir -p "${POSTGRES_BACKUP_DIR}"
+  pod=$(kubectl get pod -l "${POSTGRES_POD_LABEL}" -n "${POSTGRES_NAMESPACE}" -o name)
+  kubectl exec -it -n "${POSTGRES_NAMESPACE}" "$pod" -- pg_dumpall > "${POSTGRES_BACKUP_DIR}/postgres_$(date +%y%m%d%H%M).sql"
+}
+
+##################################################
 # Main function of script.
 # Globals:
 #   REMARK_BACKUP_DIR
+#   POSTGRES_BACKUP_DIR
 # Arguments:
 #   None
 ##################################################
 main() {
   backup_remark42 || exit 1
   cleanup "${REMARK_BACKUP_DIR}" || exit 1
+
+  backup_postgres || exit 1
+  cleanup "${POSTGRES_BACKUP_DIR}" || exit 1
 }
 
 # Entrypoint
